12fa43583a03d96f995de9511a75d1a41d5a528f
Omeans Team <47584746+omeansteam@users.noreply.github.com>
2023-10-15 16:48:50 +0700

    s

---
+++ _site/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>aaaaaaaaaa</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+</body>
+</html>
